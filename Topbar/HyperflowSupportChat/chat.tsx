eb2999f645768f1fe7d400511a399a264c319ed7
Fagner Rodrigues Damacena <fagner@Juggernaut-a>
2024-09-30 15:53:20 -0300

    corrige formatação

--- Topbar/HyperflowSupportChat/chat.tsx
+++ Topbar/HyperflowSupportChat/chat.tsx
@@ -22,25 +22,23 @@ const HyperflowSupportChat: React.FC<SupportChatProps> = ({ getJwtPromise, curre
   }
 
   Hyperflow.init(chatToken).on('getStarted', async () => {
-
     try {
-        const jwt = await getJwtPromise;
-        const params: HyperflowParams = {
-          id: currentUser.id,
-          name: currentUser.name.split(' ')[0],
-          email: currentUser.email,
-          sender: jwt,
-          origin: hyperflowConfig.origin
-        };
-    
-        if (currentUser?.isAccessPolicy) {
-          params.original_id = currentUser?.originalUserId;
-          params.original_name = currentUser?.originalUserName?.split(' ')[0];
-          params.original_email = currentUser?.originalUserEmail;
-        }
-    
-        Hyperflow.initFlow(hyperflowConfig.flowId, params);
+      const jwt = await getJwtPromise;
+      const params: HyperflowParams = {
+        id: currentUser.id,
+        name: currentUser.name.split(' ')[0],
+        email: currentUser.email,
+        sender: jwt,
+        origin: hyperflowConfig.origin
+      };
 
+      if (currentUser?.isAccessPolicy) {
+        params.original_id = currentUser?.originalUserId;
+        params.original_name = currentUser?.originalUserName?.split(' ')[0];
+        params.original_email = currentUser?.originalUserEmail;
+      }
+
+      Hyperflow.initFlow(hyperflowConfig.flowId, params);
     } catch {
       console.error('Error getting JWT');
     }
