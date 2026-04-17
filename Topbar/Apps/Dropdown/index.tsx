4837b9f400fb92c5b6dcc78286cd18ef553498fd
Vitor Maracajá <vitorvmrs@hotmail.com>
2026-04-17 16:00:42 -0300

    fix(apps-menu): improve styles

--- Topbar/Apps/Dropdown/index.tsx
+++ Topbar/Apps/Dropdown/index.tsx
@@ -54,7 +54,7 @@ const AppsDropdown = memo<AppsDropdownProps>(
       >
         <div className='uizz-layout-box-border uizz-layout-flex uizz-layout-h-14 uizz-layout-w-full uizz-layout-items-center uizz-layout-justify-between uizz-layout-border-0 uizz-layout-border-b uizz-layout-border-solid uizz-layout-border-gray-200 uizz-layout-px-4 uizz-layout-py-2 dark:uizz-layout-border-neutral-800 sm:uizz-layout-px-7'>
           <h5 className='!uizz-layout-mb-0 !uizz-layout-mt-0  uizz-layout-text-sm uizz-layout-font-normal'>
-            Lista de Apps
+            Navegue entre os Apps
           </h5>
           <button
             id='close-apps-button'
@@ -73,14 +73,14 @@ const AppsDropdown = memo<AppsDropdownProps>(
           )}
           {applicationsCategories?.map(category => (
             <section
-              className='uizz-layout-mb-4 uizz-layout-grid uizz-layout-grid-cols-1 uizz-layout-flex-wrap uizz-layout-justify-items-center uizz-layout-gap-0 uizz-layout-p-0 uizz-layout-px-4 sm:uizz-layout-mb-2 sm:uizz-layout-grid-cols-2 sm:uizz-layout-gap-6 sm:uizz-layout-p-6 lg:uizz-layout-grid-cols-3 xl:uizz-layout-grid-cols-4'
+              className='uizz-layout-mb-4 uizz-layout-grid uizz-layout-grid-cols-1 uizz-layout-flex-wrap uizz-layout-justify-items-center uizz-layout-gap-0 uizz-layout-p-0 uizz-layout-px-4 sm:uizz-layout-mb-2 sm:uizz-layout-grid-cols-2 sm:uizz-layout-gap-4 sm:uizz-layout-p-6 lg:uizz-layout-grid-cols-3 xl:uizz-layout-grid-cols-4'
               key={category.title}
             >
               <header className='uizz-layout-col-span-1 uizz-layout-w-full uizz-layout-py-4 sm:uizz-layout-col-span-2 sm:uizz-layout-p-0 lg:uizz-layout-col-span-3 xl:uizz-layout-col-span-4'>
-                <h5 className='!uizz-layout-mb-1 !uizz-layout-mt-0 uizz-layout-w-full uizz-layout-text-base uizz-layout-font-normal uizz-layout-text-content-title'>
+                <h5 className='!uizz-layout-mb-[0.125rem] !uizz-layout-mt-0 uizz-layout-w-full uizz-layout-text-base uizz-layout-font-medium uizz-layout-text-content-title'>
                   {category.title}
                 </h5>
-                <p className='uizz-layout-mt-0 uizz-layout-w-full uizz-layout-text-sm uizz-layout-text-content-subtitle'>
+                <p className='uizz-layout-mt-0 uizz-layout-w-full !uizz-layout-text-sm uizz-layout-text-content-subtitle'>
                   {category.description}
                 </p>
               </header>
@@ -95,8 +95,10 @@ const AppsDropdown = memo<AppsDropdownProps>(
                       'uizz-layout-relative uizz-layout-box-border uizz-layout-flex uizz-layout-w-full uizz-layout-cursor-pointer uizz-layout-flex-col uizz-layout-items-start uizz-layout-gap-1 uizz-layout-border-0 uizz-layout-border-b uizz-layout-border-solid uizz-layout-border-gray-200 uizz-layout-px-0 uizz-layout-py-4 !uizz-layout-text-content-caption uizz-layout-no-underline last-of-type:first-of-type:uizz-layout-border-b-0 visited:uizz-layout-text-inherit hover:!uizz-layout-bg-content-title/[0.03] hover:uizz-layout-text-inherit dark:uizz-layout-border-neutral-800 dark:hover:!uizz-layout-bg-content-title/[0.06] sm:uizz-layout-rounded sm:uizz-layout-border sm:uizz-layout-px-4 sm:last-of-type:first-of-type:uizz-layout-border-b',
                       'max-sm:before:uizz-layout-absolute max-sm:before:uizz-layout-inset-y-0 max-sm:before:-uizz-layout-left-4 max-sm:before:uizz-layout-w-4 max-sm:before:uizz-layout-content-[""] max-sm:hover:before:uizz-layout-bg-content-title/[0.03] dark:max-sm:hover:before:uizz-layout-bg-content-title/[0.06]',
                       'max-sm:after:uizz-layout-absolute max-sm:after:uizz-layout-inset-y-0 max-sm:after:-uizz-layout-right-4 max-sm:after:uizz-layout-w-4 max-sm:after:uizz-layout-content-[""] max-sm:hover:after:uizz-layout-bg-content-title/[0.03] dark:max-sm:hover:after:uizz-layout-bg-content-title/[0.06]',
-                      isCurrent &&
-                        '!uizz-layout-bg-content-title/[0.03] dark:!uizz-layout-bg-content-title/[0.06] max-sm:before:uizz-layout-bg-content-title/[0.03] max-sm:after:uizz-layout-bg-content-title/[0.03] dark:max-sm:before:uizz-layout-bg-content-title/[0.06] dark:max-sm:after:uizz-layout-bg-content-title/[0.06]'
+                      {
+                        'uizz-layout-border-primary !uizz-layout-bg-[rgba(var(--eduzz-theme-primary-rgb),0.05)] dark:!uizz-layout-bg-[rgba(var(--eduzz-theme-primary-rgb),0.05)] max-sm:before:uizz-layout-bg-content-title/[0.03] max-sm:after:uizz-layout-bg-content-title/[0.03] dark:max-sm:before:uizz-layout-bg-content-title/[0.06] dark:max-sm:after:uizz-layout-bg-content-title/[0.06]':
+                          isCurrent
+                      }
                     )}
                     key={app.application}
                     href={isCurrent ? undefined : buildRedirectUrl(app)}
@@ -104,15 +106,12 @@ const AppsDropdown = memo<AppsDropdownProps>(
                     target='_blank'
                     onClick={isCurrent ? onClose : undefined}
                   >
-                    <img
-                      src={app.icon}
-                      className='uizz-layout-mb-2 uizz-layout-block uizz-layout-max-h-6 uizz-layout-max-w-6'
-                    />
+                    <img src={app.icon} className='uizz-layout-mb-2 uizz-layout-block uizz-layout-max-h-6' />
 
                     <p className='uizz-layout-m-0 !uizz-layout-mb-0 uizz-layout-text-left uizz-layout-text-base uizz-layout-leading-normal uizz-layout-text-content-title'>
                       {app.label}
                     </p>
-                    <p className='uizz-layout-col-span-2 uizz-layout-m-0 !uizz-layout-mb-0 uizz-layout-mt-0 uizz-layout-block uizz-layout-overflow-hidden uizz-layout-text-base uizz-layout-leading-normal uizz-layout-text-content-subtitle uizz-layout-opacity-80 sm:uizz-layout-col-span-1 sm:uizz-layout-col-start-2'>
+                    <p className='uizz-layout-col-span-2 uizz-layout-m-0 !uizz-layout-mb-0 uizz-layout-mt-0 uizz-layout-block uizz-layout-overflow-hidden uizz-layout-text-sm uizz-layout-leading-normal uizz-layout-text-content-subtitle uizz-layout-opacity-80 sm:uizz-layout-col-span-1 sm:uizz-layout-col-start-2'>
                       {app.description}
                     </p>
                   </a>
